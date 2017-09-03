2149b2aa206f4f86552934aaba8c7a1bac11f865
Grayson Croom <grayson.croom@gmail.com>
2017-09-02 21:44:15 -0500

    Implemented function isInfixOf.

--- Data.List.hs
+++ Data.List.hs
@@ -1,3 +1,8 @@
+import Data.List (tails)
+
 nub' :: [a] -> [a]
 nub' [] = []
 nub' (x:xs) = x : nub' $ filter (/= x) xs
+
+isInfixOf' :: (Eq a) => a -> a -> Bool
+isInfixOf' xs = foldr1 (&&) . filter (isPrefixOf xs) . tails
